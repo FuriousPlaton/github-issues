b2717829365a316deaebc6c458bb2bd81bdccadd
Baba Jaga <85198775+d1f1@users.noreply.github.com>
2022-03-28 14:17:53 +0200

    endpoint #GET api/search returns list of repos and users randomly.

--- github-issues/pages/api/search.ts
+++ github-issues/pages/api/search.ts
@@ -1,36 +1,5 @@
 import {NextApiRequest, NextApiResponse} from "next";
-// import { ISearchData } from "../interfaces/searchData";
-import {formatResults} from "next/dist/lib/eslint/customFormatter";
-import {ISearchRepo} from "../interfaces/searchData";
-
-class Search {
-    data: any;
-    constructor(data: any) {
-        this.data = data;
-    }
-    searchData: any = [];
-    addData(data: any): void {
-        for(let res of data){
-            this.data.push(res)
-        }
-        this.searchData.push({ data });
-    }
-
-    // searchData(
-    //     data: ISearchData,
-    //     keywords: ISearchData,
-    // ) {
-    //     for (const prop in data) {
-    //         if (typeof data[prop] == 'object' && data[prop] !== null) {
-    //             this.searchData(data[prop], keywords);
-    //         }
-    //         if (Object.keys(keywords).includes(prop)) {
-    //             this.weatherKeywords[keywords[prop]](data[prop]);
-    //         }
-    //     }
-    // }
-
-}
+import {ISearchRepo, ISearchUser} from "../interfaces/searchData";
 
 async function getSearchRepo(req: NextApiRequest) {
     const res = await fetch(`https://api.github.com/search/repositories?q=${req.query.search}`);
@@ -43,34 +12,41 @@ async function getSearchUser(req: NextApiRequest) {
     return data.items;
 }
 
-let dataRepo: any = []
-let dataUsers: any = {}
+function randomData (dataR: any, dataU: any) {
+    // const l = Math.floor((dataR.length + dataU.length)/2)-1
+    let combineData: any = [];
+    for(let i=0; i<30; i++) {
+        const rand = Math.floor(Math.random() * (10 - 1 +1) + 1);
+        if(rand%2 === 0) {
+            const data0: ISearchRepo = {
+                name: dataR[i].name,
+                full_name: dataR[i].full_name,
+                description: dataR[i].description,
+                url: dataR[i].url,
+                stargazers_count: dataR[i].stargazers_count,
+                updated_at: dataR[i].updated_at,
+                language: dataR[i].language
+            }
+            combineData.push(data0)
+        } else {
+            const data1: ISearchUser = {
+                id: dataU[i].id,
+                full_name: dataU[i].full_name,
+                description: dataU[i].description,
+                url: dataU[i].url,
+                avatar_url: dataU[i].avatar_url,
+            }
+            combineData.push(data1)
+        }
+    }
+    return combineData;
+}
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    // const data1 = {
-    //     name: rdat.items[0].name,
-    //     fullName: rdat.items[0].full_name,
-    //     description: rdat.items[0].description,
-    //     url: rdat.items[0].url,
-    //     stars: rdat.items[0].stargazers_count,
-    //     lastModify: rdat.items[0].updated_at,
-    //     lang: rdat.items[0].language
-    // }
-    const data = await getSearchRepo(req).then();
+    const dataR = await getSearchRepo(req).then();
+    const dataU = await getSearchUser(req).then();
     if (req.method === 'GET') {
-        for(let i=0; i<data.length; i++) {
-            const data1: ISearchRepo = {
-                name: data[i].name,
-                full_name: data[i].full_name,
-                description: data[i].description,
-                url: data[i].url,
-                stargazers_count: data[i].stargazers_count,
-                updated_at: data[i].updated_at,
-                language: data[i].language
-            }
-            dataRepo.push(data1)
-        }
-        res.status(200).json(dataRepo);
+        res.status(200).json(randomData(dataR, dataU));
     } else {
         res.status(400);
     }
