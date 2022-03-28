38ffbecb9283cb01312be34ff7308ff32feef513
Baba Jaga <85198775+d1f1@users.noreply.github.com>
2022-03-28 13:34:53 +0200

    #GET api/search returns list of repos

--- github-issues/pages/api/search.ts
+++ github-issues/pages/api/search.ts
@@ -1,17 +1,77 @@
 import {NextApiRequest, NextApiResponse} from "next";
+// import { ISearchData } from "../interfaces/searchData";
+import {formatResults} from "next/dist/lib/eslint/customFormatter";
+import {ISearchRepo} from "../interfaces/searchData";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-    const data: ISearchData = {
-        "name": "Linux",
-        "fullName": "torvalds/linux",
-        "description": "Linux kernel source tree",
-        "url": "https://github.com/torvalds/linux",
-        "stars": 129309,
-        "lastModify": "2022-03-28T08:32:47Z",
-        "lang": "C"
+class Search {
+    data: any;
+    constructor(data: any) {
+        this.data = data;
     }
-    if (req.method === 'POST') {
-        res.status(200).json(data);
+    searchData: any = [];
+    addData(data: any): void {
+        for(let res of data){
+            this.data.push(res)
+        }
+        this.searchData.push({ data });
+    }
+
+    // searchData(
+    //     data: ISearchData,
+    //     keywords: ISearchData,
+    // ) {
+    //     for (const prop in data) {
+    //         if (typeof data[prop] == 'object' && data[prop] !== null) {
+    //             this.searchData(data[prop], keywords);
+    //         }
+    //         if (Object.keys(keywords).includes(prop)) {
+    //             this.weatherKeywords[keywords[prop]](data[prop]);
+    //         }
+    //     }
+    // }
+
+}
+
+async function getSearchRepo(req: NextApiRequest) {
+    const res = await fetch(`https://api.github.com/search/repositories?q=${req.query.search}`);
+    const data = await res.json();
+    return data.items;
+}
+async function getSearchUser(req: NextApiRequest) {
+    const res = await fetch(`https://api.github.com/search/users?q=${req.query.search}`);
+    const data = await res.json();
+    return data.items;
+}
+
+let dataRepo: any = []
+let dataUsers: any = {}
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    // const data1 = {
+    //     name: rdat.items[0].name,
+    //     fullName: rdat.items[0].full_name,
+    //     description: rdat.items[0].description,
+    //     url: rdat.items[0].url,
+    //     stars: rdat.items[0].stargazers_count,
+    //     lastModify: rdat.items[0].updated_at,
+    //     lang: rdat.items[0].language
+    // }
+    const data = await getSearchRepo(req).then();
+    if (req.method === 'GET') {
+        for(let i=0; i<data.length; i++) {
+            const data1: ISearchRepo = {
+                name: data[i].name,
+                full_name: data[i].full_name,
+                description: data[i].description,
+                url: data[i].url,
+                stargazers_count: data[i].stargazers_count,
+                updated_at: data[i].updated_at,
+                language: data[i].language
+            }
+            dataRepo.push(data1)
+        }
+        res.status(200).json(dataRepo);
     } else {
+        res.status(400);
     }
 }
--- github-issues/pages/interfaces/searchData.ts
+++ github-issues/pages/interfaces/searchData.ts
@@ -1,9 +1,16 @@
-interface ISearchData{
+export interface ISearchRepo{
     name: string,
-    fullName: string,
+    full_name: string,
     description: string,
     url: string,
-    stars: number,
-    lastModify: string,
-    lang: string,
+    stargazers_count: number,
+    updated_at: string,
+    language: string,
+};
+export interface ISearchUser{
+    id: number,
+    avatar_url: string,
+    url: string,
+    full_name: string,
+    description: string,
 };
